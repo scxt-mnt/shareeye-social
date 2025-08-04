ba550089d65e2ad40cf0c94ed4f1ee6f02bf7601
Scott <scottboragay06@gmail.com>
2025-08-04 15:33:20 +0800

    patch: updated with modern ui

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,18 +1,25 @@
 import NavBar from './Nav'
 import greetImage from '../assets/greet.png';
+import background from '../assets/purple.jpg'
 
 const LandingPage = () => {
   return (
     <>
       <NavBar />
-      <figure className='absolute left-[110px] top-[140px] h-auto'>
-        <img className='h-[300px]  ' src={greetImage} />
-
-        <figcaption className='font-poppins font-bold text-2xl text-violet-500'>
-          <p className='-ml-3'>Share your ideas</p>
+      <div className='w-screen absolute h-screen z-0 bg-violet-500' />
+        <figure className='absolute left-[110px] bottom-[100px] h-auto z-20'>
+        <img className='h-[300px]' src={greetImage} />
+    
+        <figcaption className='font-poppins font-bold text-2xl text-white  translate-y-20'>
+          <p className='mt-2 text-xl p-2 transform -translate-x-[90px] w-[300px] border-l-4  ' >Make friends. Stay connected. All in one <span className='text-violet-900'>Shareeye!</span></p>
+          <button className='text-xl bg-violet-600 pl-2 pr-2 rounded border'>About</button>
         </figcaption> 
+    
       </figure>
-
+      
+        <img src={background} className='h-[360px] w-[200px] bg-white rounded-6xl absolute left-[100px] rounded-full bottom-[200px] z-10 border '></img>
+        <div className='h-[500px] w-[500px] bg-violet-600 rounded-6xl absolute right-0 rounded-full top-0 z-0'></div>
+        <div className='h-[150px] w-[150px] bg-violet-600 rounded-full absolute right-0 rounded-full bottom-0 z-0'></div>
     </>
   )
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,26 +1,30 @@
 import { Link } from "react-router-dom"
 import logo from '../assets/ShareEyeLogo.png';
-import { useState } from "react";
+import {  useState } from "react";
 
 
 const Nav = () => {
   const [click, setClick] = useState<boolean>(false);
-
-  const handleClick = () => {
-    setClick(prevState => !click)
+      const handleClick = () => {
+    setClick(prevState => !prevState);
   }
+
 
   return (
     <>
-      <section className='absolute h-screen w-[200px] right-0 bg-gray-600 z-10'>
+      <section className={`absolute h-screen w-[200px] right-0 bg-violet-500 z-30 transition-transform duration-1000 ${click ? 'translate-x-0' : 'translate-x-full'} border-l border-left flex flex-col items-center`}>
 
+       <ul className='absolute top-[8rem] font-poppins font-bold text-white text-xl flex flex-col gap-6'>
+       <Link to='/SignIn'><li className='border-l-2 cursor-pointer p-1'>Sign in</li></Link> 
+        <Link to='/SignUp'><li className='border-l-2  cursor-pointer p-1'>Sign up</li>
+       </Link></ul>
       </section>
 
 
-      <nav className="bg-transparent w-screen h-[70px] absolute bg-violet-400 shadow-sm shadow-black flex items-center ">
+      <nav className="bg-transparent w-screen h-[70px] absolute bg-violet-400 shadow-md shadow-violet-900 flex items-center z-30">
         <img src={logo} className='h-[100px] -mt-2 ml-2' />
         <button onClick={handleClick} className='text-6xl absolute right-3 cursor-pointer bg-violet-100 pb-5 h-[50px] rounded-3xl'>
-          <h1 className='transform -translate-y-[11.5px] text-blue-500'>←</h1></button>
+          <h1 className={`transform -translate-y-[11.5px] text-blue-500 transition-transform duration-1000 ${click ? 'rotate-180 translate-y-[2px]' : 'rotate-0 -translate-y-[11px]'}`}>←</h1></button>
       </nav>
 
     </>
