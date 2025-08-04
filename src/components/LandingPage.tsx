ad470428aaa4001c04fd64c2c2937a230690fc8d
Scott <scottboragay06@gmail.com>
2025-08-04 15:54:46 +0800

    changes: website logo and name

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -6,8 +6,8 @@ const LandingPage = () => {
   return (
     <>
       <NavBar />
-      <div className='w-screen absolute h-screen z-0 bg-violet-500' />
-        <figure className='absolute left-[110px] bottom-[100px] h-auto z-20'>
+      <div className='w-screen absolute h-screen z-0 bg-violet-500 '/>
+        <figure className='absolute left-[110px] top-[140px] h-auto z-20'>
         <img className='h-[300px]' src={greetImage} />
     
         <figcaption className='font-poppins font-bold text-2xl text-white  translate-y-20'>
@@ -17,7 +17,8 @@ const LandingPage = () => {
     
       </figure>
       
-        <img src={background} className='h-[360px] w-[200px] bg-white rounded-6xl absolute left-[100px] rounded-full bottom-[200px] z-10 border '></img>
+      
+        <img src={background} className='h-[360px] w-[200px] bg-white rounded-6xl absolute left-[100px] rounded-full top-[123px] z-10 border '></img>
         <div className='h-[500px] w-[500px] bg-violet-600 rounded-6xl absolute right-0 rounded-full top-0 z-0'></div>
         <div className='h-[150px] w-[150px] bg-violet-600 rounded-full absolute right-0 rounded-full bottom-0 z-0'></div>
     </>
