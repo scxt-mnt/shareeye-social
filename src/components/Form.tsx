266f112dfcb407899c2d359591ebf3e410f26c2e
Scott <scottboragay06@gmail.com>
2025-08-08 22:13:33 +0800

    added: animation ui for form.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,11 +2,15 @@ import { useSelector } from "react-redux"
 import type { RootState } from "../Store";
 import { useDispatch } from "react-redux";
 import { setUser } from "../userSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { FormAbout } from "../publicInstance";
 const Form = () => {
     const selector = useSelector((state: RootState) => state.user.value);
     const dispatch = useDispatch();
+    const [nameClick, setNameClick] = useState<boolean>(false);
+    const [lNameClick, setLnameClick] = useState<boolean>(false);
+    const [BioClick, setBioClick] = useState<boolean>(false);
+
 
 
     useEffect(() => {
@@ -15,7 +19,7 @@ const Form = () => {
             const getToken = async () => {
                 const resGet = await FormAbout.get('/');
                 if (resGet.status === 200) {
-                    dispatch(setUser({ id: resGet.data.id, isLog: resGet.data.isLog}))
+                    dispatch(setUser({ id: resGet.data.id, isLog: resGet.data.isLog }))
                 }
             }
             getToken();
@@ -26,22 +30,41 @@ const Form = () => {
     }, [])
 
 
+    const handleNameFocus = () => {
+        setNameClick(true);
+    }
+    const handleNameBlur = () => {
+        setNameClick(false);
+    }
+    const handleLnameFocus = () => {
+        setLnameClick(true);
+    }
+    const handleLnameBlur = () => {
+        setLnameClick(false);
+    }
+    const handleBioFocus = () => {
+        setBioClick(true);
+    }
+    const handleBioBlur = () => {
+        setBioClick(false);
+    }
+
     return (
         <>
 
             {selector.isLog ? <main className='absolute h-screen w-screen grid place-items-center font-poppins text-lg'>
                 <h1 className='text-2xl font-bold'>{`Please tell us about yourself`}</h1>
-                <section className='transform '>
-                    <h1>first name</h1>
-                    <input className='w-[13rem] ' type='text' /></section>
+                <section className='h-[100px]'>
+                    <h1 className={`pointer-events-none  transition-transform  duration-1000 ${nameClick ? 'translate-y-0 transiton-text-md' : 'translate-y-[26px] text-sm text-gray-500'} `}>first name</h1>
+                    <input onFocus={handleNameFocus} onBlur={handleNameBlur} className='nameForm w-[13rem]' type='text' /></section>
                 <br />
-                <section className='-translate-y-[150px]'>
-                    <h1>last name</h1>
-                    <input className='w-[13rem] h-[2rem]' type='text' />
+                <section className=' transform -translate-y-[200px]'>
+                    <h1 className={`pointer-events-none transition-transform  duration-1000 ${lNameClick ? 'translate-y-0 transiton-text-md' : 'translate-y-[26px] text-sm text-gray-500'} `}>last name</h1>
+                    <input onFocus={handleLnameFocus} onBlur={handleLnameBlur} className='w-[13rem] h-[2rem]' type='text' />
                 </section>
                 <section className='-translate-y-[220px]'>
-                    <h1>bio</h1>
-                    <textarea className='w-[13rem] h-[8rem]'></textarea></section>
+                    <h1 className={`pointer-events-none transition-transform  duration-1000 ${BioClick ? 'translate-y-0 transiton-text-md' : 'translate-y-[26px] text-sm text-gray-500'} `}>bio</h1>
+                    <textarea onFocus={handleBioFocus} onBlur={handleBioBlur} className='w-[13rem] h-[8rem]'></textarea></section>
             </main>
                 : <h1>auth error</h1>
 
