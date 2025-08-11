69013d575356371fe92de0e472ddb21ea016ff18
Scott <scottboragay06@gmail.com>
2025-08-11 15:43:59 +0800

    added: animations for form ui forms

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -9,12 +9,12 @@ const Form = () => {
     const dispatch = useDispatch();
     const [nameClick, setNameClick] = useState<boolean>(false);
     const [lNameClick, setLnameClick] = useState<boolean>(false);
-    const [BioClick, setBioClick] = useState<boolean>(false);
+    const [bioClick, setBioClick] = useState<boolean>(false);
 
 
 
     useEffect(() => {
-        console.log('hello')
+         console.log('hello')
         try {
             const getToken = async () => {
                 const resGet = await FormAbout.get('/');
@@ -59,17 +59,17 @@ const Form = () => {
                 <h1 className='text-2xl 
                     font-bolds  top-10 font-bold text-white whitespace-pre border-b-4 border-violet-300 z-20 w-20'>{`Please tell us 
 about yourself`}</h1>
-                <section className='border-l-4 border-t-4 border-white  z-20 h-[380px] relative flex flex-col justify-center gap-5 pr-[10px] pl-[10px]  '>
-                    <section className='border-r-4 border-white  border-white  h-[85px] w-[300px] pl-5 pt-3 -mb-4 z-20   relative '>
+                <section className='  border-t-4 border-white  z-20 h-[380px] relative flex flex-col justify-center gap-5 pr-[7px] pl-[10px]  '>
+                    <section className={`border-r-[6px] transition-border duration-1000 border-white  border-white  h-[85px] w-[300px] pl-5 pt-3 -mb-4 z-20   relative ${nameClick ? 'border-violet-900' : 'border-white'}`}>
                         <h1 className={`pointer-events-none  transition-transform duration-1000 ml-2 absolute z-20 left-[30px] ${nameClick ? 'transiton-text-md text-violet-100 -translate-y-[9px]' : 'translate-y-[22px] text-sm text-gray-500'} `}>first name</h1>
                         <input onFocus={handleNameFocus} onBlur={handleNameBlur} className='nameForm w-[15rem] rounded outline-none absolute left-[27px] top-[30px] hover:border-violet-900 ' type='text' /></section>
                     <br />
-                    <section className={`border-l-4  h-[85px] w-[300px] pl-5  pt-2 z-20 relative `}>
+                    <section className={`border-l-[6px] transition-border duration-1000 h-[85px] w-[300px] pl-5  pt-2 z-20 relative ${lNameClick ? 'border-violet-900' : 'border-white'}`}>
                         <h1 className={`pointer-events-none transition-transform duration-1000 ml-2 z-20 absolute left-[32px] ${lNameClick ? '-translate-y-[6px] transiton-text-md text-violet-100 text-white' : 'translate-y-[26px] text-sm text-gray-500'} `}>last name</h1>
                         <input onFocus={handleLnameFocus} onBlur={handleLnameBlur} className='w-[15rem] h-[30px] outline-none absolute left-[30px] z-0 top-[30px] rounded' type='text' />
                     </section>
-                    <section className='border-l-4 border-white  h-[110px] w-[300px] pl-5 pt-3 z-20 relative'>
-                        <h1 className={`pointer-events-none transition-transform  duration-1000 ml-2 z-10 absolute left-[30px] ${BioClick ? '-translate-y-[15px] transiton-text-md text-violet-100' : 'translate-y-[35px] text-sm text-gray-500'} `}>bio</h1>
+                    <section className={`border-l-[6px] transition-border duration-1000 h-[110px] w-[300px] pl-5 pt-3 z-20 relative ${bioClick ? 'border-violet-900' : 'border-white'} `}>
+                        <h1 className={`pointer-events-none transition-transform  duration-1000 ml-2 z-10 absolute left-[30px] ${bioClick ? '-translate-y-[15px] transiton-text-md text-violet-100' : 'translate-y-[35px] text-sm text-gray-500'} `}>bio</h1>
                         <textarea onFocus={handleBioFocus} onBlur={handleBioBlur} className='w-[15rem] h-[4rem] outline-none absolute left-[30px] z-0 top-[25px]'></textarea></section></section>
 
             </main>
