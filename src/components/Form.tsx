24a83ab022706c993da2c610919b7446e814e3f7
Scott <scottboragay06@gmail.com>
2025-08-11 15:24:48 +0800

    update: modern ui for form sec

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -53,22 +53,22 @@ const Form = () => {
     return (
         <>
 
-            {selector.isLog ? <main className='absolute h-screen w-screen grid place-content-center font-poppins text-lg bg-violet-400'>
-                <div className='bg-violet-700 h-[300px] w-[300px] absolute rounded-full z-0'></div>
-                <div className='bg-violet-700 h-[200px] w-[200px] absolute rounded-full z-0 bottom-0 right-0'></div>
+            {selector.isLog ? <main className='absolute h-screen w-screen grid place-content-center font-poppins text-lg bg-violet-400 gap-4'>
+                <div className=' border-r-4 bg-violet-700 h-[300px] w-[300px] absolute rounded-full z-0'></div>
+                <div className=' border-l-4 bg-violet-800 h-[200px] w-[200px] absolute rounded-full z-0 bottom-0 right-0'></div>
                 <h1 className='text-2xl 
-                    font-bolds absolute left-[20px] top-10 font-bold text-white whitespace-pre border-b-4 border-violet-300'>{`Please tell us 
+                    font-bolds  top-10 font-bold text-white whitespace-pre border-b-4 border-violet-300 z-20 w-20'>{`Please tell us 
 about yourself`}</h1>
-                <section className='z-20 h-[380px] bg-violet-900 relative flex flex-col justify-center gap-5 pr-[10px] pl-[10px] rounded-lg'>
-                    <section className='bg-violet-700 h-[85px] w-[300px] pl-5 rounded-xl pt-3 -mb-4 z-20   relative '>
+                <section className='border-l-4 border-t-4 border-white  z-20 h-[380px] relative flex flex-col justify-center gap-5 pr-[10px] pl-[10px]  '>
+                    <section className='border-r-4 border-white  border-white  h-[85px] w-[300px] pl-5 pt-3 -mb-4 z-20   relative '>
                         <h1 className={`pointer-events-none  transition-transform duration-1000 ml-2 absolute z-20 left-[30px] ${nameClick ? 'transiton-text-md text-violet-100 -translate-y-[9px]' : 'translate-y-[22px] text-sm text-gray-500'} `}>first name</h1>
-                        <input onFocus={handleNameFocus} onBlur={handleNameBlur} className='nameForm w-[15rem] rounded outline-none absolute left-[27px] top-[30px] ' type='text' /></section>
+                        <input onFocus={handleNameFocus} onBlur={handleNameBlur} className='nameForm w-[15rem] rounded outline-none absolute left-[27px] top-[30px] hover:border-violet-900 ' type='text' /></section>
                     <br />
-                    <section className='bg-violet-700 h-[85px] w-[300px] pl-5 rounded-xl pt-2 z-20 relative'>
+                    <section className={`border-l-4  h-[85px] w-[300px] pl-5  pt-2 z-20 relative `}>
                         <h1 className={`pointer-events-none transition-transform duration-1000 ml-2 z-20 absolute left-[32px] ${lNameClick ? '-translate-y-[6px] transiton-text-md text-violet-100 text-white' : 'translate-y-[26px] text-sm text-gray-500'} `}>last name</h1>
                         <input onFocus={handleLnameFocus} onBlur={handleLnameBlur} className='w-[15rem] h-[30px] outline-none absolute left-[30px] z-0 top-[30px] rounded' type='text' />
                     </section>
-                    <section className='bg-violet-700 h-[110px] w-[300px] pl-5 rounded-xl pt-3 z-20 relative'>
+                    <section className='border-l-4 border-white  h-[110px] w-[300px] pl-5 pt-3 z-20 relative'>
                         <h1 className={`pointer-events-none transition-transform  duration-1000 ml-2 z-10 absolute left-[30px] ${BioClick ? '-translate-y-[15px] transiton-text-md text-violet-100' : 'translate-y-[35px] text-sm text-gray-500'} `}>bio</h1>
                         <textarea onFocus={handleBioFocus} onBlur={handleBioBlur} className='w-[15rem] h-[4rem] outline-none absolute left-[30px] z-0 top-[25px]'></textarea></section></section>
 
@@ -78,6 +78,9 @@ about yourself`}</h1>
             }
 
         </>
+
+
+
     )
 }
 
