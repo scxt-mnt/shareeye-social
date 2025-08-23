e0e265d907a7c96c3ae77c0e13bc2cebb5a21ede
Scott <scottboragay06@gmail.com>
2025-08-23 17:22:31 +0800

    fix boilerplate code to form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -61,31 +61,12 @@ const Form = () => {
         }
     }
 
+    const fields = [
+        { name: "name", setFocus: setNameClick, clickValue: nameClick,value: name, setValue: setName},
+        { name: "last name", setFocus: setLnameClick,clickValue: lNameClick, value: lastName, setValue: setLastName},
+        { name: "bio", setFocus: setBioClick,clickValue: bioClick, value: aboutBio, setValue: setAboutBio}
+    ]
 
-    const handleNameFocus = () => {
-        setNameClick(true);
-    }
-    const handleNameBlur = () => {
-        if (!name) {
-            setNameClick(false);
-        }
-    }
-    const handleLnameFocus = () => {
-        setLnameClick(true);
-    }
-    const handleLnameBlur = () => {
-        if (!lastName) {
-            setLnameClick(false);
-        }
-    }
-    const handleBioFocus = () => {
-        setBioClick(true);
-    }
-    const handleBioBlur = () => {
-        if (!aboutBio) {
-            setBioClick(false);
-        }
-    }
 
     return (
         <>
@@ -93,22 +74,19 @@ const Form = () => {
             {selector.isLog ? <main className='absolute h-screen w-screen grid place-content-center font-poppins text-lg bg-violet-400 gap-4'>
                 <div className='border-r-4 bg-violet-700 h-[300px] w-[300px] absolute rounded-full z-0'></div>
                 <LoadingAnimation ref={loading} />
-                    <h1 className='text-2xl 
+                <h1 className='text-2xl 
                     font-bolds  top-10 font-bold text-white whitespace-pre border-b-4 border-violet-300 z-20 w-20'>{`Please tell us 
 about yourself`}</h1>
                 <section className='border-t-4 border-white  z-20 h-[380px] relative flex flex-col justify-center gap-5 pr-[7px] pl-[10px]  '>
-                    <section className={`border-r-[6px] transition-border duration-1000 h-[85px] w-[300px] pl-5 pt-3 -mb-4 z-20   relative ${nameClick ? 'border-violet-900' : 'border-white'}`}>
-                        <h1 className={`pointer-events-none  transition-transform duration-1000 ml-2 absolute z-20 left-[30px] ${nameClick ? 'transiton-text-md text-violet-100 -translate-y-[9px]' : 'translate-y-[22px] text-sm text-gray-500'} `}>first name</h1>
 
-                        <input value={name} onChange={(e) => setName(e.target.value)} onFocus={handleNameFocus} onBlur={handleNameBlur} className='nameForm w-[15rem] rounded outline-none absolute left-[27px] top-[30px] hover:border-violet-900 z-10 pl-2' type='text' /></section>
-                    <br />
-                    <section className={`border-l-[6px] transition-border duration-1000 h-[85px] w-[300px] pl-5  pt-2 z-20 relative ${lNameClick ? 'border-violet-900' : 'border-white'}`}>
-                        <h1 className={`pointer-events-none transition-transform duration-1000 ml-2 z-20 absolute left-[32px] ${lNameClick ? '-translate-y-[6px] transiton-text-md text-violet-100 text-white' : 'translate-y-[26px] text-sm text-gray-500'} `}>last name</h1>
-                        <input value={lastName} onChange={(e) => setLastName(e.target.value)} onFocus={handleLnameFocus} onBlur={handleLnameBlur} className='w-[15rem] h-[30px] outline-none absolute left-[30px] z-0 top-[30px] rounded pl-2' type='text' />
-                    </section>
-                    <section className={`border-l-[6px] transition-border duration-1000 h-[110px] w-[300px] pl-5 pt-3 z-20 relative ${bioClick ? 'border-violet-900' : 'border-white'} `}>
-                        <h1 className={`pointer-events-none transition-transform  duration-1000 ml-2 z-10 absolute left-[30px] ${bioClick ? '-translate-y-[15px] transiton-text-md text-violet-100' : 'translate-y-[35px] text-sm text-gray-500'} `}>bio</h1>
-                        <textarea value={aboutBio} onChange={(e) => setAboutBio(e.target.value)} onFocus={handleBioFocus} onBlur={handleBioBlur} className='w-[15rem] h-[4rem] outline-none absolute left-[30px] z-0 top-[25px] pl-2'></textarea></section></section>
+                    {fields.map((field) => {
+                        return (
+                            <section key={field.name} className={`border-r-[6px] transition-border duration-1000 h-[85px] w-[300px] pl-5 pt-3 -mb-4 z-20   relative ${field.clickValue ? 'border-violet-900' : 'border-white'}`}>
+                                <h1 className={`pointer-events-none  transition-transform duration-1000 ml-2 absolute z-20 left-[30px] ${field.clickValue ? 'transiton-text-md text-violet-100 -translate-y-[9px]' : 'translate-y-[22px] text-sm text-gray-500'} `}>{field.name}</h1>
+                                <input value={field.value} onChange={(e) => field.setValue(e.target.value)} onFocus={() => field.setFocus(true)} onBlur={() => !field.value && field.setFocus(false)} className='nameForm w-[15rem] rounded outline-none absolute left-[27px] top-[30px] hover:border-violet-900 z-10 pl-2' type='text' /></section>
+                        )
+                    })}
+                </section>
                 <Link to="Profile-Upload"> <button onClick={aboutUserSend} className={` w-[1px] transition-all duration-1000 absolute right-0 bottom-10 font-bold text-white bg-violet-500  ${name && lastName && aboutBio ? 'pr-[5rem] pl-[2rem]' : ''}`}>next</button></Link>
             </main>
                 : <h1>auth error</h1>
