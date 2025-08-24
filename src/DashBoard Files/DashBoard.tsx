54c679831240923973f9ed99aa75f2058d4dc7b6
Scott <scottboragay06@gmail.com>
2025-08-24 17:08:45 +0800

    layout: post section for newsfeed

--- src/DashBoard Files/DashBoard.tsx
+++ src/DashBoard Files/DashBoard.tsx
@@ -1,14 +1,23 @@
 import type { RootState } from "../Store"
 import { useSelector } from "react-redux"
 import DashBoardNav from "./DashBoardNav"; 
-
+import { useEffect } from "react";
 const DashBoard = () => {
   const selector = useSelector((state: RootState) => state.user.value);
+  const profileSelector = useSelector((state: RootState) => state.profile.value);
+
+  useEffect(() => {
+    
+  },[])
   return (
     <>
       {selector.isLog ? <>
       <DashBoardNav />
-      <h1>{`HELLO! ${selector.username}`}</h1> 
+      <section className='w-screen h-20 shadow mt-2 flex flex-row items-center gap-4 shadow-lg'>
+        <img src='' className='w-[3rem] h-[3rem] bg-white ml-5 rounded-full border border-violet-900'></img>
+        <input placeholder='post something' type='text' className='pl-10 p-1  rounded-full outline-none placeholder:m-[10rem]'/>
+      </section>
+      <h1>{profileSelector.name}</h1> 
       </>
         : <h1>{`Authentication error`}</h1>
         }
--- src/DashBoard Files/DashBoardNav.tsx
+++ src/DashBoard Files/DashBoardNav.tsx
@@ -1,9 +1,11 @@
-
+import logo from '../assets/ShareEyeLogoCircle.png'
 
 const DashBoardNav = () => {
     return (
         <>
-            <main className='w-full h-[55px] bg-violet-700'></main>
+            <main className='w-full h-[55px] bg-violet-700'>
+                <img src={logo} alt='image didnt reload' className='w-[2.5rem] ml-[5px]'/>
+            </main>
         </>
     )
 }
