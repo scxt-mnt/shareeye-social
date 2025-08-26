4ab10aa02d9dee12690d6aed92f7f3c2849bd505
Scott <scottboragay06@gmail.com>
2025-08-26 17:02:16 +0800

    added: toggle button options

--- src/Forms/Posting.tsx
+++ src/Forms/Posting.tsx
@@ -1,26 +1,29 @@
 import { useState } from "react"
 
 const Posting = () => {
-    const [textIsClicked, setTextIsClicked] = useState<Boolean>(false);
-    const [imageIsClicked, setImageIsClicked] = useState<boolean>(false);
+
+    const [activate, setActive] = useState<'text' | 'images'>('text')
+
 
     const buttons = [
-        { name: "Text" },
-        { name: "Images" }
-    ]
+        { name: "Text", value: 'text' },
+        { name: "Images", value: 'images' }
+    ] as const;
+
     return (
         <>
 
             <main className='w-screen h-screen grid place-content-center gap-5 relative '>
-                <section className='flex gap-2 w-auto relative left-[8rem] bg-violet text-white font-poppins font-bold '>
+                <section className='flex gap-2 w-auto justify-self-end bg-violet-500 text-white font-poppins font-bold rounded p-2'>
                     {buttons.map((fields, index) => {
-                        return(
-                        <button key={index} className='pt-[2px] pb-[2px] pr-[20px] pl-[20px] bg-violet-500 rounded-xl'>{fields.name}</button>)
+                        return (
+                            <button key={index} onClick={() => setActive(fields.value)} className={`pt-[2px] pb-[2px] pr-[20px] pl-[20px] rounded-xl ${activate === fields.value ? 'bg-violet-400' : 'bg-violet-500'}`}>{fields.name}</button>)
                     })}
                 </section>
                 <section className="w-[20rem] h-[25rem] bg-white shadow-2xl rounded-2xl ">
 
                 </section>
+                
             </main>
         </>
     )
